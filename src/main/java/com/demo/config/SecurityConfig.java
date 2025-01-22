d47dce90845302ee70782c6716f239e47411de26
Giannela Huamani <giannelahc@gmail.com>
2025-01-22 19:34:42 +0100

    cors config

--- src/main/java/com/demo/config/SecurityConfig.java
+++ src/main/java/com/demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.demo.config;
 
+import io.lettuce.core.ZAddArgs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,9 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -26,7 +30,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(con -> con.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         //.requestMatchers("/api/authenticate").permitAll()
                         //.requestMatchers("/api/login").permitAll() // Permitir acceso sin autenticación a /api/login
@@ -37,6 +41,19 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // Ajusta según tu frontend
+        configuration.addAllowedMethod("*"); // O puedes especificar los métodos: GET, POST, etc.
+        configuration.addAllowedHeader("*"); // Permitir todos los encabezados
+        configuration.setAllowCredentials(true); // Si necesitas permitir credenciales
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
